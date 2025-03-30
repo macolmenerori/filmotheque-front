29de49fefec94d9a933611f9dd645b12a72b4f6b
macolmenerori <miguelangelcolmenero@gmail.com>
2025-03-30 15:35:47 +0200

    Upgrade react-router v7

--- src/pages/MainPage/MoviesTable/MoviesTable.tsx
+++ src/pages/MainPage/MoviesTable/MoviesTable.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router';
 
 import { api } from '../../../api';
 import BackedUpChip from '../../../common/components/Chips/BackedUpChip/BackedUpChip';
